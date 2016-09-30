c08fc69024adbee4ba50ebcde3d1f3facaa21a6e
pixis <allaboutblender@gmail.com>
2016-09-29 21:03:21 -0400

    Renamed observeValueEvent and added mapChildrenTo

--- rxfirebase/src/main/java/com/kelvinapps/rxfirebase/KotlinExtensions.kt
+++ rxfirebase/src/main/java/com/kelvinapps/rxfirebase/KotlinExtensions.kt
@@ -4,10 +4,16 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.Query
 import rx.Observable
 
-fun Query.observeValueEvent(): Observable<DataSnapshot> {
+fun Query.observe(): Observable<DataSnapshot> {
     return RxFirebaseDatabase.observeValueEvent(this)
 }
 
 fun <T> Observable<DataSnapshot>.mapTo(clazz: Class<T>): Observable<T> {
     return map({ it.getValue(clazz) })
+}
+
+fun <T> Observable<DataSnapshot>.mapChildrenTo(clazz: Class<T>): Observable<List<T>> {
+    return flatMap {
+        Observable.from(it.children).mapTo(clazz).toList()
+    }
 }
